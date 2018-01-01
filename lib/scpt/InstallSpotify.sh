ec9a6ed86ba3c1b0c7c8fd1034579bc0040d992f
Karl <local-part@domain>
2018-01-02 00:44:50 +0100

    [.files/lib/scpt/] InstallSpotify.sh updated.

--- lib/scpt/InstallSpotify.sh
+++ lib/scpt/InstallSpotify.sh
@@ -7,7 +7,7 @@ mkdir -p /Users/${USER}/Temporary
 cd /Users/${USER}/Temporary
 # Add user temporary folder, and change working directory to it.
 
-curl -L -O http://download.spotify.com/Spotify.dmg
+curl --remote-name --location http://download.spotify.com/Spotify.dmg # `--remote-name --location`, `-O -L` or `-OL`... But it's a shell script so let's get verbose. (i) 'Short version options that don't need any additional values can be used immediately next to each other, like for example you can specify all the options -O, -L and -v at once as -OLv.' (cURL manual) https://web.archive.org/web/20180101231613/https://curl.haxx.se/docs/manpage.html
 hdiutil mount -nobrowse Spotify.dmg
 cp -R /Volumes/Spotify/Spotify.app /Applications/
 hdiutil unmount /Volumes/Spotify
@@ -15,5 +15,5 @@ mv Spotify.dmg /Users/${USER}/Downloads/
 # Download and install Spotify.
 
 cd /Users/${USER}
-rm -r Temporary
+rmdir Temporary
 # Remove user temporary folder.
