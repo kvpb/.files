a723b6d1cecaa7d25eeb9a08f6595b74769bba9b
Karl <local-part@domain>
2018-01-02 00:44:36 +0100

    [.files/lib/scpt/] InstallChrome.sh updated.

--- lib/scpt/InstallChrome.sh
+++ lib/scpt/InstallChrome.sh
@@ -7,13 +7,13 @@ mkdir -p /Users/${USER}/Temporary
 cd /Users/${USER}/Temporary
 # Add user temporary folder, and change working directory to it.
 
-curl -L -O https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
+curl --remote-name --location https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg
 hdiutil mount -nobrowse googlechrome.dmg
 cp -R /Volumes/Google\ Chrome/Google\ Chrome.app /Applications/
 hdiutil unmount /Volumes/Google\ Chrome
 mv googlechrome.dmg ~/Downloads/
 # Download Chrome, and install it.
 
-cd /Users/${USER}
-rm -r Temporary
+cd ..
+rmdir Temporary
 # Remove user temporary folder.
