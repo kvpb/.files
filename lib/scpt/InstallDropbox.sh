9b25c01d471ced8c611eff1c3088ac8f0834550c
Karl <local-part@domain>
2018-01-02 00:43:56 +0100

    [.files/lib/scpt/] InstallDropbox.sh updated.

--- lib/scpt/InstallDropbox.sh
+++ lib/scpt/InstallDropbox.sh
@@ -7,13 +7,14 @@ mkdir -p /Users/${USER}/Temporary
 cd /Users/${USER}/Temporary
 # Add user temporary folder, and change working directory to it.
 
-curl -L -O https://www.dropbox.com/download?plat=mac
+curl --location --remote-name https://www.dropbox.com/download?plat=mac
 hdiutil mount -nobrowse Dropbox*.dmg
 cp -R /Volumes/Dropbox\ Installer/Dropbox.app ./ && open Dropbox.app
 hdiutil unmount /Volumes/Dropbox\ Installer
-mv Dropbox*.dmg /Users/${USER}/Downloads/
+#mv Dropbox*.dmg /Users/${USER}/Downloads/
+mv Dropbox*.dmg /Users/${USER}/.Trash/
 # Download and install Dropbox.
 
 cd /Users/${USER}
-rm -r Temporary
+rmdir Temporary
 # Remove user temporary folder.
