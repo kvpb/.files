290215fc107cf91f4fb8a407f5b83970139d53fa
Karl <local-part@domain>
2018-01-17 18:33:18 +0100

    [.files/lib/scpt/] InstallVLCMediaPlayer.sh added.

---
+++ lib/scpt/InstallVLCMediaPlayer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# InstallVLCMediaPlayer.sh
+# Install VLC 2.2.8 for Mac OS X
+
+mkdir -p /Users/${USER}/Temporary
+cd /Users/${USER}/Temporary
+# Add user temporary folder, and change working directory to it.
+
+curl --remote-name --location https://get.videolan.org/vlc/last/macosx/vlc-2.2.8.dmg
+hdiutil mount -nobrowse vlc-2.2.8.dmg
+cp -R /Volumes/vlc-2.2.8/VLC.app /Applications/
+hdiutil unmount /Volumes/vlc-2.2.8
+mv vlc-2.2.8.dmg /Users/${USER}/Downloads/
+# Download VLC 2.2.8, and install it.
+
+cd /Users/${USER}
+rmdir Temporary
+# Remove user temporary folder.
