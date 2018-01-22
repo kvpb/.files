bc69d081b7604c311c36a65d5f494bd36d34fac7
Karl <local-part@domain>
2018-01-22 19:19:13 +0100

    [.files/lib/scpt/] InstallGitforMac.sh updated.

--- lib/scpt/InstallGitforMac.sh
+++ lib/scpt/InstallGitforMac.sh
@@ -3,12 +3,24 @@
 # InstallGitforMac.sh
 # Install Git for Mac latest
 
+#DMG_filename="git-latest-osx-installer.dmg"
+#DMG_filename_extension="${DMG_filename##*.}"
+#DMG_filename_without_extension=$(basename "${DMG_filename}" .dmg)
+#DMG_PKG_pathname="/Volumes/Git * Intel Universal/git-*-intel-universal-*.pkg"
+#DMG_PKG_filename="${DMG_PKG_pathname##*/}"
+
 mkdir -p /Users/${USER}/Temporary
 cd /Users/${USER}/Temporary
 curl --remote-header-name --location https://sourceforge.net/projects/git-osx-installer/files/latest --output git-latest-osx-installer.dmg
 hdiutil mount -nobrowse git-latest-osx-installer.dmg
+#DMG_PKG_filename=$(basename "/Volumes/Git\ *\ *\ Intel\ Universal/git-*-intel-universal-*.pkg")
+#DMG_PKG_filename_extension="${DMG_PKG_filename##*.}"
+#DMG_PKG_filename="${DMG_PKG_filename%.*}"
+#DMG_PKG_filename_without_extension=$(basename "/Volumes/Git\ *\ *\ Intel\ Universal/git-*-intel-universal-*.pkg" .pkg)
 sudo installer -pkg /Volumes/Git\ *\ Intel\ Universal/git-*-intel-universal-*.pkg -target /
 hdiutil unmount /Volumes/Git\ *\ Intel\ Universal
+#mv "${DMG_filename}" "${DMG_PKG_filename_without_extension}.${DMG_filename_extension}"
+#mv git-*-intel-universal-*.dmg ~/Downloads/
 mv git-latest-osx-installer.dmg ~/Downloads/
 cd /Users/${USER}
 rmdir Temporary
