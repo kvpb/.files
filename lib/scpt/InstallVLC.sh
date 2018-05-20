8088a27f0ae3ac24f896bbc17742ed4988a68814
Karl <local-part@domain>
2018-05-20 06:59:00 +0200

    [.files/lib/scpt/] InstallVLC.sh updated.

--- lib/scpt/InstallVLC.sh
+++ lib/scpt/InstallVLC.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 # InstallVLC.sh
-# Install VLC 2.2.8 for Mac OS X
+# Install VLC Media Player for Mac OS X
 
 mkdir -p /Users/${USER}/Temporary
 cd /Users/${USER}/Temporary
 # Add user temporary folder, and change working directory to it.
 
-curl --remote-name --location https://get.videolan.org/vlc/last/macosx/vlc-2.2.8.dmg
-hdiutil mount -nobrowse vlc-2.2.8.dmg
-cp -R /Volumes/vlc-2.2.8/VLC.app /Applications/
-hdiutil unmount /Volumes/vlc-2.2.8
-mv vlc-2.2.8.dmg /Users/${USER}/Downloads/
-# Download VLC 2.2.8, and install it.
+curl --remote-name --location http://download.videolan.org/pub/videolan/vlc/3.0.0/macosx/vlc-3.0.0.dmg
+hdiutil mount -nobrowse vlc-3.0.0.dmg # (i) Before version 3, VLC's DMG volume name was the DMG file's. From version 3 onwards, it's 'VLC media player'. That broke the script. I shall rewrite all these scripts to watch for a new directory in /Volumes then use it in a variable.
+cp -R /Volumes/VLC\ media\ player/VLC.app /Applications/
+hdiutil unmount /Volumes/VLC\ media\ player
+mv vlc-3.0.0.dmg /Users/${USER}/Downloads/
+# Download VLC Media Player, and install it.
 
 cd /Users/${USER}
 rmdir Temporary
