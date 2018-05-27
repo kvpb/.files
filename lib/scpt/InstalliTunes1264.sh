4c0836943f4560f2bb211b00b03ff6d962e7c6a7
Karl <local-part@domain>
2018-05-27 16:43:34 +0200

    [.files/lib/scpt/] InstalliTunes1264.sh made. [.files/] .functions updated: computerandomnumber & generaterandomusername BASH functions created.

---
+++ lib/scpt/InstalliTunes1264.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# InstalliTunes1264.sh
+# Install iTunes 12.6.4 Mac
+
+sudo -v # Ask for an administrator password.
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null & # Update sudo timestamp until the script is over.
+mkdir -p ${HOME}/Temporary && cd ${HOME}/Temporary # Make a user's temporary folder, and go to it.
+curl -O -L https://secure-appldnld.apple.com/itunes12/091-60759-201803029-1F70CB08-3131-11E8-9791-31052B2AA206/iTunes12.6.4.dmg # Download the iTunes 12.6.4 disk image.
+hdiutil mount -nobrowse iTunes12.6.4.dmg # Mount the iTunes 12.6.4 disk image.
+cd /Volumes/iTunes # Go to the mounted iTunes 12.6.4 disk image.
+sudo installer -pkg Install\ iTunes.pkg -target / # Install iTunes 12.6.4.
+cd .. # Go back to the mounted volumes' directory.
+hdiutil unmount /Volumes/iTunes # Unmount the iTunes 12.6.4 disk image.
+mv iTunes12.6.4.dmg ${HOME}/Downloads/ # Move the iTunes 12.6.4 disk image to the user's downloads folder.
+cd ${HOME} && rmdir Temporary # Go to the user's home directory, and delete his temporary folder.
