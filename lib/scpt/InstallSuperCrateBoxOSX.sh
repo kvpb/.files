5baa5a9f4d0f8e4b6e021015ffd7015f8766893c
Karl <local-part@domain>
2018-06-10 19:46:48 +0200

    [.files/lib/scpt/] InstallSuperCrateBox.sh created.

---
+++ lib/scpt/InstallSuperCrateBoxOSX.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# InstallSuperCrateBoxOSX.sh
+# Install Super Crate Box Mac
+
+cd ${HOME}
+mkdir Temporary
+cd Temporary
+curl --remote-name --remote-header-name --location --silent http://supercratebox.com/download/osx.zip
+unzip osx.zip -d /Applications
+tar -c -f "Super Crate Box OS X.tar" osx.zip
+mv "Super Crate Box OS X.tar" ../Downloads/
+rm osx.zip
+cd ..
+rmdir Temporary
