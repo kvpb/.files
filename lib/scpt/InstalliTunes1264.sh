b417b98c5ff0d0e3e4294ac4a7398363253ac48b
Karl <local-part@domain>
2018-06-10 15:56:58 +0200

    [.files/lib/scpt/] InstalliTunes1264.sh updated.

--- lib/scpt/InstalliTunes1264.sh
+++ lib/scpt/InstalliTunes1264.sh
@@ -6,7 +6,7 @@
 sudo -v # Ask for an administrator password.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null & # Update sudo timestamp until the script is over.
 mkdir -p ${HOME}/Temporary && cd ${HOME}/Temporary # Make a user's temporary folder, and go to it.
-curl -O -L https://secure-appldnld.apple.com/itunes12/091-60759-201803029-1F70CB08-3131-11E8-9791-31052B2AA206/iTunes12.6.4.dmg # Download the iTunes 12.6.4 disk image.
+curl --remote-name --location https://secure-appldnld.apple.com/itunes12/091-60759-201803029-1F70CB08-3131-11E8-9791-31052B2AA206/iTunes12.6.4.dmg # Download the iTunes 12.6.4 disk image.
 hdiutil mount -nobrowse iTunes12.6.4.dmg # Mount the iTunes 12.6.4 disk image.
 cd /Volumes/iTunes # Go to the mounted iTunes 12.6.4 disk image.
 sudo installer -pkg Install\ iTunes.pkg -target / # Install iTunes 12.6.4.
