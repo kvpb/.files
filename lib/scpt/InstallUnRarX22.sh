d301b0afd2b46f3a52c4cb2eab6af2b8e8bb6c74
Karl <local-part@domain>
2019-03-17 14:06:58 +0100

    [.files/lib/scpt/] Install{UnRarX22,RAR550forMacOSX}.sh updated: comment typo corrected.

--- lib/scpt/InstallUnRarX22.sh
+++ lib/scpt/InstallUnRarX22.sh
@@ -8,5 +8,5 @@ cd ${HOME}/Temporary # Set the current working directory to the current user's u
 curl --remote-name --location --insecure https://web.archive.org/web/20060814154224/http://www.unrarx.com/files/UnRarX_2.2.zip # Download the UnRARX zip archive.
 unzip UnRarX_2.2.zip -d /Applications # Unzip the UnRARX zip archive to the system applications' directory.
 mv -v UnRarX_2.2.zip ${HOME}/Downloads/ # Move the UnRARX zip archive to the current user's downloads folder.
-cd ${HOME} # Set the current corking directory back to the current user's home folder.
+cd ${HOME} # Set the current working directory back to the current user's home folder.
 rmdir Temporary # Delete the current user's unofficial temporary folder.
