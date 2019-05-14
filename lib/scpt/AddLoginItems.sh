a1b383956840cddbeee1ac691f10efb094342f02
Karl <local-part@domain>
2019-05-14 13:30:03 +0200

    [.files/lib/scpt/] AddLoginItems.sh written. [Boy, am I satisfied of this one. I get better and better.]

---
+++ lib/scpt/AddLoginItems.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+for i in "$@";
+do
+	var=$(osascript -e "on run({i})" -e "set variable to POSIX path of (path to application i)" -e "end" -- "${i}"); # -e "on run(argv)"
+	until [ ! -z "$(pgrep ${i})" ];
+	do
+		sleep 1;
+	done;
+	killall "${i}"; #printf "${var%/}\tdebug\n";
+	var=${var%/}; #printf "${var}\tdebug\n";
+
+	osascript \
+		-e "on run({var})" \
+		-e 'tell application "System Events" to make login item at end with properties {path:var, hidden:false}' \
+		-e 'return display notification var with title "Login Item Added"' \
+		-e "end" \
+		-- "${var}";
+done;
