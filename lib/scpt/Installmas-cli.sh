c6b072b38ec87feb15b073e04cbc96379876c06c
Karl <local-part@domain>
2019-06-13 14:31:31 +0200

    [.files/lib/scpt/] InstallXcodeCommandLineTools.sh, Installfswatch.sh & Installmas-cli.sh updated: erased some syntax errors, proofread overall scripts, and rewrote the conditional variable declaration accordingly to my new style.
    I shouldn't write when I'm having an attention failure. Obviously, I'm not 27 yet. But hey, I'm still overpowering this already! That crap's gonna end, but before it does, it's gonna follow me TO INFINITY AND BEYOND! SQWALALAAAAAAAAAAAAAAAAA.. NS SOM PARTY MN BRAV MYLOO

--- lib/scpt/Installmas-cli.sh
+++ lib/scpt/Installmas-cli.sh
@@ -2,9 +2,9 @@
 
 # function() # 'Download artifacts from a latest GitHub release with bash and PowerShell' by Mark Vincze on july 9, 2016 (https://web.archive.org/web/20180131223312/https://blog.markvincze.com/download-artifacts-from-a-latest-github-release-in-sh-and-powershell/), custom by Karl Bertin on 2018.1.31, 22:30:00
 #{
-	local latestrelease=$(curl -L -s -H 'Accept: application/json' https://github.com/mas-cli/mas/releases/latest)
-	local latestversion=$(echo $latestrelease | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/') # The releases are returned in the format {"id":3622206,"tag_name":"hello-1.0.0.11",...}, we have to extract the tag_name.
-	local latestreleaseURL="https://github.com/mas-cli/mas/releases/download/${latestversion}/mas-cli.zip"
+	latestrelease=$(curl -L -s -H 'Accept: application/json' https://github.com/mas-cli/mas/releases/latest)
+	latestversion=$(echo $latestrelease | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/') # The releases are returned in the format {"id":3622206,"tag_name":"hello-1.0.0.11",...}, we have to extract the tag_name.
+	latestreleaseURL="https://github.com/mas-cli/mas/releases/download/${latestversion}/mas-cli.zip"
 #}
 
 mkdir -p /Users/${USER}/Temporary
