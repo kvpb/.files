32b41abe5a315ef5f10a2eec4ce050c6cbfc7ded
Karl <local-part@domain>
2019-07-18 00:13:17 +0200

    [.files/lib/scpt/] InstallSFMono.sh updated: patched the errors '8: [: -ge:' and '11: [: -lt: unexpected operator'.

--- lib/scpt/InstallSFMono.sh
+++ lib/scpt/InstallSFMono.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 kernel=$(uname -s)
+[ "${kernel}" = 'Linux' ] && firmwareversion=0
 [ "${kernel}" = 'Darwin' ] && firmwareversion=$(defaults read loginwindow SystemVersionStampAsString | cut -d. -f2- | cut -f1,1 -d'.')
 wd=$(pwd)
-n=$[RANDOM%9999+1]
+n=$[RANDOM%99+1]
 
 if [ "${kernel}" = 'Darwin' -a ${firmwareversion} -ge 12 ]
 then
@@ -12,17 +13,17 @@ elif [ "${kernel}" = 'Linux' -o "${kernel}" = 'Darwin' -a ${firmwareversion} -lt
 then
 	mkdir -p ${HOME}/tmp/dir${n} && cd ${HOME}/tmp/dir${n}
 	curl --remote-name --remote-header-name --location "https://www.dropbox.com/s/rcu7hh16ayl5y6k/SFMono.tar?dl=0" #--output SFMono.tar
-	tar -xvf SFMono.tar
-	#mv Fonts/*.otf /Library/Fonts/ # (i) System fonts folder: /Library/Fonts/; User fonts folder: ~/Library/Fonts/
 	if [ "${kernel}" = 'Darwin' ]
 	then
+		tar -xvf SFMono.tar
+		#mv Fonts/*.otf /Library/Fonts/ # (i) System fonts folder: /Library/Fonts/; User fonts folder: ~/Library/Fonts/
 		mv ${HOME}/tmp/dir${n}/Fonts/*.otf /Library/Fonts/
-		mv SFMono.tar ${HOME}/Downloads/
 	elif [ "${kernel}" = 'Linux' ]
 	then
-		mv ${HOME}/tmp/dir${n}/Fonts/*.otf /usr/share/fonts/opentype/
-		mv SFMono.tar ${HOME}/Downloads/
+		mkdir /usr/share/fonts/sanfrancisco
+		tar -xvf SFMono.tar Fonts/*.otf -C /usr/share/fonts/opentype
 	fi # assuming the user runs on a common, stock, unlocalized distro. [Now that's some kwality skreeptin. But I program for myself, and I need fast and efficient code, so if one wants to get it working, they gotta RTFM.]
+	mv SFMono.tar ${HOME}/Downloads/
 	rm -r Fonts
 	cd ..
 	rm -r tmp${n}
