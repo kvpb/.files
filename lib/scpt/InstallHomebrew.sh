a6d313c0b93b6dcadd0073386fcc4934c1fe54ab
Karl <local-part@domain>
2020-02-01 15:46:04 +0100

    [.files/lib/scpt/] InstallHomebrew.sh updated: turn off analytics.

--- lib/scpt/InstallHomebrew.sh
+++ lib/scpt/InstallHomebrew.sh
@@ -2,8 +2,11 @@
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew analytics off
+
 # InstallHomebrew.sh
 # Install Homebrew (https://brew.sh)
+# & Turn Analytics Off (https://docs.brew.sh/Analytics)
 # 
 # Rémi Prévost, Mike McQuaid & Danielle Lalonde
 # Website: https://github.com/Homebrew/brew
