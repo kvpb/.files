d17a82ed655f6de53fa04552ccf2fc0d4dd128fb
Karl <local-part@domain>
2020-02-01 15:30:44 +0100

    DeleteCreativeCloudFilesFromUserHome.sh updated.

--- lib/scpt/DeleteCreativeCloudFilesFromUserHome.sh
+++ lib/scpt/DeleteCreativeCloudFilesFromUserHome.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash
 
-cd ${HOME}
+#[ ! -e ${HOME}/Creative\ Cloud\ Files ] && exit
 
-if [ -d Creative\ Cloud\ Files ]; then
-	if [ -L Creative\ Cloud\ Files ]; then
-		rm Creative\ Cloud\ Files
+#n=$[RANDOM%99+1] && while [ -e ${HOME}/Temporary/Folder${n} ]; do n=$[RANDOM%99+1]; done;
+
+if [ -d ${HOME}/Creative\ Cloud\ Files ]
+then
+	if [ -L ${HOME}/Creative\ Cloud\ Files ]
+	then
+		rm ${HOME}/Creative\ Cloud\ Files
 	else
-		if [ $(ls -A Creative\ Cloud\ Files) ]; then
-			mv Creative\ Cloud\ Files/* ${HOME}/Desktop/
+		if [ $(ls -A ${HOME}/Creative\ Cloud\ Files) ]
+		then
+			n=$[RANDOM%99+1] && while [ -e ${HOME}/Temporary/Folder${n} ]; do n=$[RANDOM%99+1]; done;
+			mkdir -p ${HOME}/Temporary/Folder${n}
+			mv ${HOME}/Creative\ Cloud\ Files/* ${HOME}/Temporary/Folder${n}/
 			rmdir Creative\ Cloud\ Files
 		else
 			rmdir Creative\ Cloud\ Files
 		fi
 	fi
-fi
-
-if [ ! -d Creative\ Cloud\ Files ]; then
+else
 	exit
 fi
