f528c58e7567c6b77781091453d471e886cb5720
Karl <local-part@domain>
2020-02-07 13:22:29 +0100

    [.files/lib/scpt/] ResetmacOSWindows.sh updated: added support for iTerm2.

--- lib/scpt/ResetmacOSWindows.sh
+++ lib/scpt/ResetmacOSWindows.sh
@@ -2,4 +2,5 @@
 
 defaults delete com.apple.Terminal "NSWindow Frame ${ProfileName}" # Reset the Terminal windows.
 defaults delete com.apple.Safari "NSWindow Frame BrowserWindowFrame" # Reset the Safari windows.
+for i in {0..99}; do defaults delete com.googlecode.iterm2 "NSWindow Frame iTerm Window ${i}"; done; # Reset the iTerm2 windows.
 defaults write com.spotify.client "NSWindow Frame com.spotify.client.mainwindow" = "0 251 1030 626 0 0 1440 877 " # Set the Spotify windows.
