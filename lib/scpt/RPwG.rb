c93d856d0785547a8e93ea3af8add1bd67864a81
Karl <local-part@domain>
2020-02-10 17:11:50 +0100

    [.files/lib/scpt/] RPwG.rb 1.21 updated to version 1.22: rewrote the header as a footer.

--- lib/scpt/RPwG.rb
+++ lib/scpt/RPwG.rb
@@ -1,16 +1,5 @@
 #!/usr/bin/env ruby
 
-# RPwG (Random Password Generator)
-# RPwG.rb, rpwg.rb
-# Version 1.2
-# 
-# Karl 'Kabe' Bertin `kvpb`
-# Email: local-part@domain
-# LinkedIn: https://www.linkedin.com/in/karlbertin
-# Facebook: 
-# Instagram: https://www.instagram.com/karlbertin/
-# Snapchat: https://www.snapchat.com/add/karlbertin
-
 character_set = [ "!", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~" ] # ASCII order, no 032, """, "\" or "#".
 qwazerty_character_set = [ "!", "$", "%", "&", "'", "(", ")", "*", ",", "-", ".", "/", ":", ";", "?", "@", "A", "M", "Q", "W", "Z", "[", "]", "^", "`", "a", "m", "q", "w", "z", "{", "|", "}", "~" ]
 
@@ -35,3 +24,14 @@ while password_length > 0 do
 end
 
 puts `tput setaf 1` + "Randomly generated password:" + `tput sgr0` + " " + password.map(&:inspect).join('').to_s.gsub('"', '') + "\n"
+
+# RPwG.rb
+# RPwG (Random Password Generator)
+# Version 1.22
+# 
+# Karl Bertin `kvpb`
+# Email: local-part@domain
+# LinkedIn: https://www.linkedin.com/in/karlbertin
+# Facebook: 
+# Instagram: https://www.instagram.com/karlbertin/
+# Snapchat: https://www.snapchat.com/add/karlbertin
