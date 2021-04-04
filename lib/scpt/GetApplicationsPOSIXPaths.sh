6a9f88c903f563965b2ff3ea87f731e1c5256219
Karl <local-part@domain>
2021-04-04 16:43:31 +0200

    [.files/lib/scpt/] GetApplicationsPOSIXPaths.sh updated.

--- lib/scpt/GetApplicationsPOSIXPaths.sh
+++ lib/scpt/GetApplicationsPOSIXPaths.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
+#exec 2>> ${HOME}/Temporary/log
+#set -xv
 
 for var in "${@}";
 do
-
-osascript -- - "${var}" <<'EOF'
-	on run({var})
-		POSIX path of (path to application var)
-	end
-EOF
-
+	osascript -- - "${var}" <<-'EOF'
+		on run({var})
+			POSIX path of (path to application var)
+		end
+	EOF
+	# Wait. What? https://unix.stackexchange.com/a/76483
 done;
+exit 0;
+
+# GetApplicationsPOSIXPaths.sh
+# 
+# Karl `kvpb`
+# Telephone: +33 A BB BB BB BB
+# Email: local-part@domain
