9f008562e734d53a4a1adf39d67d8934387e497d
Karl <local-part@domain>
2021-11-30 03:56:38 +0100

    Portfile.sh updated.

--- Portfile.sh
+++ Portfile.sh
@@ -9,8 +9,13 @@ do
 done 2>/dev/null &
 
 sudo port selfupdate
-for i in  bash zsh  ruby nodejs14 nodejs16 octave  xhyve  openssh openssl mosh  blueutil mas  git  curl wget  tmux  vim  ffmpeg  fswatch htop tree thefuck  ;
+for i in  bash zsh  ruby nodejs14 nodejs16 octave  xhyve  openssh openssl mosh  blueutil mas  git  curl wget  tmux  vim  ffmpeg  fswatch htop tree thefuck  coreutils findutils grep;
 do
 	sudo port install ${i}
 done
+
+sudo port select --set python2 python27
+sudo port select --set python3 python39
+sudo port load openssh
+
 exit 0
