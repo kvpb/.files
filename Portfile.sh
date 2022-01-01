7718eccd8cd3f855cadfe460d74a372bd2ae0ad4
Karl <local-part@domain>
2022-01-01 15:30:00 +0100

    Portfile.sh updated.

--- Portfile.sh
+++ Portfile.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env sh
 
 sudo -v
-while true;
+while true
 do
-	sudo -n true;
-	sleep 60;
-	kill -0 "$$" || exit;
+	sudo -n true
+	sleep 60
+	kill -0 ${$} || exit
 done 2>/dev/null &
-
-sudo port selfupdate
-for i in  bash zsh  ruby nodejs14 nodejs16 octave  xhyve  openssh openssl mosh  blueutil mas  git  curl wget  tmux  vim  ffmpeg  fswatch htop tree thefuck  coreutils findutils grep;
+sudo port -q selfupdate
+for s in  bash zsh  python310 python27 ruby nodejs16 nodejs14 octave jq  xhyve  openssh openssl mosh  blueutil mas  git  curl wget  tmux  vim  ffmpeg  fswatch htop tree thefuck  coreutils findutils grep  #bash zsh  rust python27 python310 ruby nodejs14 nodejs16 octave jq  xhyve  openssh openssl mosh  blueutil mas  git  curl wget  tmux  vim  ffmpeg  fswatch htop tree thefuck  coreutils findutils grep
 do
-	sudo port install ${i}
+	#sudo port -f uninstall "${s}"
+	sudo port -q install "${s}"
 done
-
-sudo port select --set python2 python27
-sudo port select --set python3 python39
-sudo port load openssh
-
+sudo port -q select --set python python310
+sudo port -q select --set python3 python310
+sudo port -q select --set python2 python27
+sudo port -q load openssh
+#sudo port -N selfupdate && sudo port -N upgrade outdated; sudo port -N reclaim
 exit 0
