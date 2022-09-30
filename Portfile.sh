25a217c003a5778690b7645415bc85ae3365b67e
Karl <local-part@domain>
2022-09-30 15:26:52 +0200

    Portfile.sh updated.

--- Portfile.sh
+++ Portfile.sh
@@ -8,7 +8,55 @@ do
 	kill -0 ${$} || exit
 done 2>/dev/null &
 sudo port -q selfupdate
-for s in  bash zsh  rust go python310 python27 ruby31 nodejs18 octave jq  xhyve  openssh openssl mosh  blueutil mas  git  curl wget  tmux  vim  ffmpeg  fswatch htop tree thefuck  coreutils findutils grep # To hell with outdated garbage!
+for s in \
+	\
+	bash \
+	zsh \
+	\
+	`#rust` \
+	go \
+	python310 \
+	python27 \
+	`#ruby31` \
+	nodejs18 \
+	octave \
+	jq \
+	sqlite3 \
+	\
+	`#ruby-build` \
+	`#rbenv` \
+	`#pyenv` \
+	\
+	xhyve \
+	\
+	openssh \
+	openssl \
+	mosh \
+	\
+	blueutil \
+	mas \
+	\
+	git \
+	\
+	curl \
+	wget \
+	\
+	tmux \
+	\
+	vim \
+	\
+	ffmpeg \
+	\
+	fswatch \
+	htop \
+	tree \
+	thefuck \
+	\
+	coreutils \
+	findutils \
+	grep \
+	\
+	# To hell with outdated garbage!
 do
 	#sudo port -f uninstall "${s}"
 	sudo port -q install "${s}"
@@ -16,7 +64,7 @@ done
 sudo port -q select --set python python310
 sudo port -q select --set python3 python310
 sudo port -q select --set python2 python27
-sudo port -q select --set ruby ruby31
+#sudo port -q select --set ruby ruby31
 sudo port -q load openssh
 #sudo port -N selfupdate && sudo port -N upgrade outdated; sudo port -N reclaim
 exit 0
