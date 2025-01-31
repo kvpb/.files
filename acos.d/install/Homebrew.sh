dda4db28b375dc23a0f28a1d5147d90987c60c8e
Karl <local-part@domain>
2025-01-31 19:11:22 +0100

    (.files/acos.d/install/) Homebrew.sh deleted.

--- acos.d/install/Homebrew.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env zsh
-
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/kvpb/.bash_profile
-eval "$(/opt/homebrew/bin/brew shellenv)"
-# Download and install Homebrew. https://brew.sh/
